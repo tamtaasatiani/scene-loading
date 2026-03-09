1df85a296e7a14ebe1bc13ecadbbec3bbc455746
Tamta Asatiani <tamta@geolab.edu.ge>
2026-03-09 11:46:06 +0400

    Improve singletonmonobehaviour

--- Assets/Scripts/SingletonMonobehaviour.cs
+++ Assets/Scripts/SingletonMonobehaviour.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static T Instance;
-    
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy (Instance);
-        }
+    private static T _instance;
 
-        Instance = this as T;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+            
+            _instance = FindFirstObjectByType<T>();
+            
+            if (_instance != null)
+            {
+                return _instance;
+            }
+
+            SetupInstance();
+            return _instance;
+        }
+    }
+    
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+    
+    private static void SetupInstance()
+    {
+        _instance = FindFirstObjectByType<T>();
+        
+        if (_instance != null)
+            return;
+        
+        GameObject gameObj = new GameObject();
+        gameObj.name = typeof(T).Name;
+        _instance = gameObj.AddComponent<T>();
+        DontDestroyOnLoad(gameObj);
+    }
+    
+    private void RemoveDuplicates()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
