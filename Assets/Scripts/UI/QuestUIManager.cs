cadbf0ebd37242d44a8b3a8f38ff58f6f7a692cf
Tamta Asatiani <31993618+tamtaasatiani@users.noreply.github.com>
2026-04-05 19:49:08 +0400

    Add serialized dictionary package

--- Assets/Scripts/UI/QuestUIManager.cs
+++ Assets/Scripts/UI/QuestUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using QuestSystem;
+using QuickEye.Utility;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,28 +10,28 @@ namespace UI
     [CreateAssetMenu(menuName = "Quest System/UI/UI Manager")]
     public class QuestUIManager : ScriptableObject
     {
-        [SerializeField] private SerializedDictionary<QuestState, QuestConfig> questUIConfigs;
+        [SerializeField] private UnityDictionary<QuestState, QuestConfig> questUIConfigs = new();
         
         private List<QuestUIElement?> _uiElements;
         
         public List<QuestUIElement?> UIElements => _uiElements;
 
-        private void OnEnable()
+        private void Subscribe()
         {
             QuestManager.Instance.AddListenerPokeAllAsync(ConfigureUIElements).Forget();
             QuestManager.Instance.BroadcastPokeAllAsync().Forget();
         }
 
-        private void OnDisable()
+        private void Unsubscribe()
         {
             QuestManager.Instance.RemoveListenerPokeAllAsync(ConfigureUIElements).Forget();
         }
 
         private void ConfigureUIElements(Quest quest)
         {
-            var uiElement = questUIConfigs[quest.QuestState].Configure(quest);
-            if (uiElement != null)
-                _uiElements.Add(uiElement);
+            //var uiElement = questUIConfigs[quest.QuestState].Configure(quest);
+            //if (uiElement != null)
+            //    _uiElements.Add(uiElement);
         }
     }
 }
